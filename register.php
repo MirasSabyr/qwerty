d4ca2ae0dfd5dc4d8e0a91f55a9b55ca2e976d67
AliNurlan <149326775+AliNurlan@users.noreply.github.com>
2024-05-24 08:56:38 +0500

    reg fix

--- register.php
+++ register.php
@@ -9,36 +9,41 @@
 <body>
        
 <form method="post" action="">
-    <input type="text" class="f1" name="login" placeholder="Enter your Login">
+    <input type="text" class="f1" name="login" placeholder="Enter your Login" value="">
     <input type="password" class="f2" name="password" placeholder="Enter your Password">
     <input type="password" class="f3" name="confirm" placeholder="Confirm Password">
     <input type="submit" class="p7" value="Отправить">
 </form>
 
 <?php
-
+include "base.php";
 
 function checkPasswordStrength($password) {
   $errors = [];
 
   // Проверка на наличие хотя бы одного специального символа
-  // $patern='#[!@#$%^&*()_+=\[\]{};:,.<>?]#'
-  // if (!preg_match($patern, $password)) {
-  //   $errors[] = "Пароль должен содержать хотя бы один специальный символ. $password";
-  // }
+  $patern='/[!@#$%^&*()_+=\[\]{}\'"|\/;:,.<>?]/';
+  if (!preg_match($patern, $password)) {
+    $errors[] = "Пароль должен содержать хотя бы один специальный символ.";
+  }
 
   // Проверка на наличие хотя бы одной буквы в верхнем регистре
   if (!preg_match('/[A-ZА-ЯЁ]/', $password)) {
     $errors[] = "Пароль должен содержать хотя бы одну букву в верхнем регистре.";
   }
 
+  // Проверка на наличие хотя бы одной цифры
+  if (!preg_match('/[0-9]/', $password)) {
+    $errors[] = "Пароль должен содержать хотя бы одну цифру.";
+  }
+
   //проверка на длины пароля
   if (mb_strlen($password)<6) {
-    $errors[]="Пароль меньше шести символов.";
+    $errors[]="Пароль не меньше шести символов.";
   }
 
   if (mb_strlen($password)>20) {
-    $errors[]="Пароль больше двадцати символов.";
+    $errors[]="Пароль не больше двадцати символов.";
   }
 
   // Возвращаем массив ошибок, если они есть
@@ -60,40 +65,44 @@ $link = mysqli_connect($host, $user, $db_password, $db_name) or die(mysqli_error
 mysqli_query($link, "SET NAMES 'utf8'");
 // текст SQL запроса, который будет передан базе
 
-if (!empty($_POST['login']) and !empty($_POST['password'])) {
-    $log = $_POST['login'];
-    $pass = $_POST['password'];
-    $confirm = $_POST['confirm'];
-    if ($pass == $confirm) {
-      $result = checkPasswordStrength($pass);
-      echo "<p>$pass<p>";
-      if ($result === true) {
-        $query = "SELECT * FROM Пользователи WHERE login='$log' AND password='$pass'";
-        $res = mysqli_query($link, $query);
-        $user = mysqli_fetch_assoc($res);
+if (!empty($_POST['login']) and !empty($_POST['password'])) { 
+  $log = $_POST['login']; 
+  $pass = $_POST['password']; 
+  $confirm = $_POST['confirm']; 
+  if ($pass == $confirm) { 
+    $result = checkPasswordStrength($pass); 
+    if ($result === true) { 
+      $query = "SELECT * FROM Users WHERE login = ?";
+      $stmt = mysqli_prepare($link, $query);
+      mysqli_stmt_bind_param($stmt, 's', $log);
+      mysqli_stmt_execute($stmt);
+      $res = mysqli_stmt_get_result($stmt);
+      $user = mysqli_fetch_assoc($res);
 
-        if (empty($user)) {
+      if (empty($user)) { 
+        $query = "INSERT INTO Users(login, password) VALUES (?, ?)"; 
+        $stmt = mysqli_prepare($link, $query);
+        mysqli_stmt_bind_param($stmt, 'ss', $log, $pass);
+        mysqli_stmt_execute($stmt);
+        $_SESSION['auth'] = true; 
+        echo "Удачная регистрация.<br>"; 
+      } 
+      else{echo "Логин занят.<br>";} 
+      } 
+    else { 
+      echo "Пароль не соответствует требованиям. Ошибки: <br>"; 
+      foreach ($result as $error) { 
+        echo "- " . $error . "<br>"; 
+      } 
+    } 
+     
 
-          $query = "INSERT INTO Пользователи(login, password) VALUES ('$log','$pass')";
-          mysqli_query($link, $query);
-          $_SESSION['auth'] = true;
-          echo "Удачная регистрация.<br>";
-        }
-        else{echo "Неудачная регистрация.<br>";}
-        }
-      else {
-        echo "Пароль не соответствует требованиям. Ошибки: <br>";
-        foreach ($result as $error) {
-          echo "- " . $error . "<br>";
-        }
-      }
-      
-
-    } else {
-        echo "Пароли не совпадают.<br>";
-    }
+  } else { 
+      echo "Пароли не совпадают.<br>"; 
+  } 
 
 }
+
 
 ?>
 
