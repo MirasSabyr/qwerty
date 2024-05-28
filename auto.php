84d81fae761545db739af5e299a3cdf7cd0fc093
Miras <148694043+MirasSabyr@users.noreply.github.com>
2024-05-28 10:40:29 +0500

    pre-res4

--- auto.php
+++ auto.php
@@ -1,23 +1,5 @@
 <?php
 session_start();
-?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <link href="register.css" rel="stylesheet">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Authorization</title>
-</head>
-<body>
-     
-<form method="post" action="auto.php">
-    <input type="text" class="f1" name="login" placeholder="Вводите Логин">
-    <input type="password" class="f3" name="password" placeholder="Вводите Пароль">
-    <input type="submit" class="f4" value="Отправить">
-</form>
-
-<?php
 $host = 'localhost'; // имя хоста
 $db_name = 'Trevel_Vista'; // имя базы данных
 $user = 'root'; // имя пользователя
@@ -50,6 +32,7 @@ if (!empty($_POST['login']) and !empty($_POST['password'])) {
         // прошел авторизацию
         $_SESSION['auth'] = true;
         echo "Вы успешно авторизованы.<br>";
+        header("Location: main/index.html");
     } else {
         // неверно ввели логин или пароль
         echo "Неверный логин или пароль.<br>";
