0188637292c834cb71dea0cb3e811fc4883fdafc
Miras <148694043+MirasSabyr@users.noreply.github.com>
2024-05-29 10:12:51 +0500

    main

--- main/cait.js
+++ main/cait.js
@@ -1,3 +1,23 @@
 $(".theme-switch").on("click", () => {
     $("body").toggleClass("light-theme");
   });
+
+  window.addEventListener("load", (event) => {
+    // Проверяем значение cookie
+    const authCookie = document.cookie.split(';').find(cookie => cookie.trim().startsWith('auth='));
+    if (authCookie) {
+        const authValue = authCookie.split('=')[1];
+        if (authValue === 'true') {
+            console.log('Пользователь авторизован.');
+            // Получаем элемент кнопки
+            const signUpButton = document.getElementById('signUp');
+            signUpButton.style.display = 'none';
+            
+            // При успешной авторизации меняем цвет фона
+        } else {
+            console.log('Пользователь НЕ авторизован.');
+        }
+    } else {
+        console.log('нету печенек.');
+    }
+});
